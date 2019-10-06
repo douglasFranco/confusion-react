bd398d1ee523d2f638d7df4655fce690d1708459
Douglas Robert di Franco <douglas.rfranco@gmail.com>
2019-10-06 20:03:47 -0300

    second week complete

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,50 +1,64 @@
-import React, { Component } from 'react'
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
+import React from 'react'
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
-class Dishdetail extends Component {
-    constructor (props) {
-        super(props)
-    }
+const RenderDish = ({dish}) => {
+    return (dish &&
+        <div className="col-12 col-md-5 mt-1">
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    )
+}
 
-    renderDish(dish) {        
+const RenderComments = ({comments}) => {
+    console.log(comments)
+    let commentList = comments.map((comment) => {
         return (
-            <div className="col-12 col-md-5 mt-1">
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
-        )      
-    }
+            <ul className="list-unstyled">
+                <li>
+                    {`-- ${comment.author}, 
+                    ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}`}
+                </li>
+                <li>{`${comment.comment}`}</li>
+            </ul>
+        )
+    })
+    return (comments &&
+        <div className="col-12 col-md-5 mt-1">
+            <h4>Comments</h4>
+            {commentList}
+        </div>
+    )
+}
 
-    renderComments(comments) { 
-        let commentList = comments.map((comment) => {
-            return (
-                <ul className="list-unstyled">
-                    <li>{`-- ${comment.author}, ${comment.date}`}</li> 
-                    <li>{`${comment.comment}`}</li>
-                </ul>               
-            )
-        } ) 
-        return ( comments &&   
-            <div className="col-12 col-md-5 mt-1">
-                <h4>Comments</h4>
-                {commentList}
-            </div>
-        )   
-    }    
+const Dishdetail = (props) => {
 
-    render() {       
-        return( this.props.dish &&
-            <div  className="row">
-                {this.renderDish(this.props.dish)}
-                {this.renderComments(this.props.dish.comments)}
-            </div>            
-        )    
-    }
+    return (
+        props.dish ?
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+            : null
+    )
 }
 
 export default Dishdetail
